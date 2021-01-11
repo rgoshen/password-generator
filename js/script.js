6cc5024a5e351cdb62aa98e5cd162b6a5aa47515
rgoshen <rick.goshen@gmail.com>
2021-01-11 07:35:10 -0700

    added random functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-// starter script
+// functions for generate random chars, numbers and symbols
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomLower() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
